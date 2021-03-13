58641f77d64923e00243b1bda69821b7261536f6
Mbwebwa Lukusa <16ml250@esisalama.org>
2021-03-13 13:50:07 +0200

    Bonjour

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
-<?php
 
-echo "Bonjour le monde c'est ALEXMB250";
-
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bonjour</title>
+</head>
+<body>
+    <h1>Bonjour le monde c'est ALEXMB250</h1>
+</body>
+</html>
