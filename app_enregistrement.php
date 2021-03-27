771e502c9d3857840b7090c14ca471670eb316dd
Mbwebwa Lukusa <16ml250@esisalama.org>
2021-03-27 14:22:11 +0200

    change app

--- app_enregistrement.php
+++ app_enregistrement.php
@@ -39,7 +39,7 @@
     }
     else if($ussd_string_exploded[0] == 2 && $level == 3)
     {
-        $response = "END Merci ".$ussd_string_exploded[0]." pour vous inscrire.\nNous vous tiendrons au courant" ;
+        $response = "END Merci ".$ussd_string_exploded[1]." pour vous inscrire.\nNous vous tiendrons au courant" ;
     }
     
     
